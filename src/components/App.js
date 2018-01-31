832b328e092eab8244850a4e7b0468957e641d55
Joe Morris <jmorris@pillartechnology.com>
2018-01-31 09:42:47 -0500

    nf-001: jm - add airbnb eslint to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className='netFlow-app'>Welcome to Netflow</div>
-        )
-    }
+  render() {
+    return (
+      <div className="netFlow-app">Welcome to Netflow</div>
+    );
+  }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: [
-        path.join(__dirname, 'src', 'index.js')
-    ],
-    output: {
-        path:  `${__dirname}/dist`,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css/,
-                loaders: ['style-loader', 'css-loader']
-            }]
-    },
-    devServer: {
-        contentBase: `${__dirname}/dist`,
-        historyApiFallback: true,
-        port: 3000
-    }
+  entry: [
+    path.join(__dirname, 'src', 'index.js'),
+  ],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css/,
+        loaders: ['style-loader', 'css-loader'],
+      }],
+  },
+  devServer: {
+    contentBase: `${__dirname}/dist`,
+    historyApiFallback: true,
+    port: 3000,
+  },
 };
 
