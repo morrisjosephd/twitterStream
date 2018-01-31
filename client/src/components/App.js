1fb1759ee9b88af4356bc98cef9d5ac552e83573
Joe Morris <jmorris@pillartechnology.com>
2018-01-31 14:08:42 -0500

    basic socket.io fullstack implementation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import subscribeToTimer from '../services/api';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      timestamp: 'no timestamp yet',
+    };
+    subscribeToTimer((err, timestamp) => this.setState({ timestamp }));
   }
 
   render() {
     return (
-      <div className="netFlow-app">Welcome to Netflow</div>
+      <div className="netFlow-app">Welcome to Netflow
+        <div>This is the timer value: {this.state.timestamp}</div>
+      </div>
     );
   }
 }
