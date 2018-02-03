b88e0bca1d04fd2edd33463b89d7fb22aba5d94e
Joe Morris <morris.josephd@gmail.com>
2018-02-02 19:06:20 -0500

    removing the environment variable file..oops

--- server/config/env_variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CONSUMER_KEY=SjepS24yzEOyyxAsboQopOhI8
-export CONSUMER_SECRET=PIrnZjVnFfVFqnz0ItzL8lPtdeAzrQAZnVjtFRYZGP0tKtfvEl
-export ACCESS_TOKEN_KEY=26008755-1ceBuzVNXrY9qN2vmxT6o3fXePfV6GHO39zeT74SJ
-export ACCESS_TOKEN_SECRET=LqJ2tLcUl8AAfm45EFPzPF668BJyXnaeYXC4RWfzzUwEn
-
-echo "environment variables sourced..."
